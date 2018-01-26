630c179d8aae3d6186f8f0838d67f08873117f22
Rohit Ner <rohitner1@gmail.com>
2018-01-26 20:51:51 +0530

    multiple index assignment for method set

--- lib/daru/vector.rb
+++ lib/daru/vector.rb
@@ -1619,7 +1619,7 @@ module Daru
 
     # Works similar to #[]= but also insert the vector in case index is not valid
     # It is there only to be accessed by Daru::DataFrame and not meant for user.
-    def set indexes, val
+    def set *indexes, val
       cast(dtype: :array) if val.nil? && dtype != :array
       guard_type_check(val)
 
